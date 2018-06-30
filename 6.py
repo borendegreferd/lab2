665a1a715947c08e66f2f24971927188ea466558
borendegreferd <borendegreferd@gmail.com>
2018-06-30 18:05:42 +0500

    Initial commit

---
+++ 6.py
@@ -0,0 +1,6 @@
+'''Проверить, является ли введенный с клавиатуры символ заглавной латинской буквой?'''
+a=input()
+if a=='Q' or a=='W' or a=='T' or a=='R' or a=='P' or a=='S' or a=='D' or a=='F'or a=='G' or a=='H' or a=='J' or a=='K' or a=='L' or a=='Z' or a=='X'or a=='C' or a=='B' or a=='N' or a=='M':
+    print('Yes')
+else:
+    print('No')
